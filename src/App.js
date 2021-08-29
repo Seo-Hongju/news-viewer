6e600a7bad469acadab0b4cfad661d3c38fe9612
Seo-Hongju <tjghdwn0824@naver.com>
2021-08-29 23:26:39 +0900

    Revert "part 14.6.1 commit"
    This reverts commit 779c0fad6174ec644f0fbda022840fe311c218c7.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import React, { useCallback, useState } from 'react';
-import Categories from './components/Categories';
+import React from 'react';
 import NewsList from './components/NewsList';
 
 const App = () => {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback((category) => setCategory(category), []);
-
-  return (
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </>
-  );
-};
+  return <NewsList />;
+}
 
 export default App;
